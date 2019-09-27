7cb6e9fee83155fb60edefe11760501914fda6c8
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-09-28 00:49:49 +0800

    Level-4

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,17 +9,12 @@ class Deadline extends Task {
     }
 
     public String toString() {
-<<<<<<< HEAD
-        return ("[" + "D" + "][" + this.getStatusIcon() + "] "
-                + this.description
-                + " (by: " + this.by.showtime() + ")"
-        );
-=======
+
         return ("[" + "D" + "]["
                 + this.getStatusIcon() + "] "
                 + this.description + " (by: "
                 + this.by.showtime() + ")");
->>>>>>> 2cdf26903eca468a95ad9d9258119cf03d743147
+
     }
 
     public Time getby() {
