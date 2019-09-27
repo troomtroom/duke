66038481f55ddcea66450c3c8b847734be941d00
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-09-28 00:49:49 +0800

    Level-4
    Former-commit-id: d5c18866b9ca284c13de0e58b0d80714019b311f
    Former-commit-id: f9117c5f9b1a828bcc2c13e86adf7debcbd900ae

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,19 +10,10 @@ class Event extends Task {
 
     public String toString() {
         return ("[" + "E" + "]["
-<<<<<<< HEAD
-                + this.getStatusIcon()
-                + "] "
-                + this.description
-                + " (at: "
-                + this.at.showtime()
-                + ")"
-        );
-=======
                 + this.getStatusIcon() + "] "
                 + this.description
                 + " (at: " + this.at.showtime() + ")");
->>>>>>> 2cdf26903eca468a95ad9d9258119cf03d743147
+
     }
 
     public Time getat() {
