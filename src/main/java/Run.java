fbb80fee8d05ba55846e5f060fbe8d98775adc4d
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-09-28 16:37:07 +0800

    Add JavaDocs

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,7 +1,6 @@
 public class Run {
     public static void main(String[] args) {
-        //Level
         new Duke("/mnt/c/users/kartike/desktop/nus/semester3/cs2103/duke/data/warehouse.txt").run();
     }
 }
-//Level - 8
+//JAVADOCS complete
