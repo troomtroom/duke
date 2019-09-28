7674f83acc70c7286d9bb6d69cbb86b45462351d
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-09-28 13:02:21 +0800

    Level-7

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,6 +1,7 @@
 public class Run {
     public static void main(String[] args) {
         //Level - 6
+        //Added Level-8
         new Duke("/mnt/c/users/kartike/desktop/nus/semester3/cs2103/duke/data/warehouse.txt").run();
     }
 }
