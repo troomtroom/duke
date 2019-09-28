5b9425d0c5a07c7b06d9e4023a4f286355aa0694
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-09-28 13:58:43 +0800

    A-MoreOOP

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,11 +1,6 @@
 public class Run {
     public static void main(String[] args) {
-        //Level - 6
-<<<<<<< HEAD
-        //Added Level-8
-=======
-        //added 7
->>>>>>> branch-Level-7
+        //Level
         new Duke("/mnt/c/users/kartike/desktop/nus/semester3/cs2103/duke/data/warehouse.txt").run();
     }
 }
